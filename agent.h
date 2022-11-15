62b8af0ca6fbf7f5f054c6d69077da578eaff122
newb1er <taiwanmichael429@gmail.com>
2022-11-16 00:12:53 +0800

    chore: lint

--- agent.h
+++ agent.h
@@ -8,62 +8,70 @@
  */
 
 #pragma once
-#include <string>
-#include <random>
-#include <sstream>
-#include <map>
-#include <type_traits>
 #include <algorithm>
 #include <fstream>
-#include "board.h"
+#include <map>
+#include <random>
+#include <sstream>
+#include <string>
+#include <type_traits>
+
 #include "action.h"
+#include "board.h"
 
 class agent {
-public:
-	agent(const std::string& args = "") {
-		std::stringstream ss("name=unknown role=unknown " + args);
-		for (std::string pair; ss >> pair; ) {
-			std::string key = pair.substr(0, pair.find('='));
-			std::string value = pair.substr(pair.find('=') + 1);
-			meta[key] = { value };
-		}
-	}
-	virtual ~agent() {}
-	virtual void open_episode(const std::string& flag = "") {}
-	virtual void close_episode(const std::string& flag = "") {}
-	virtual action take_action(const board& b) { return action(); }
-	virtual bool check_for_win(const board& b) { return false; }
+ public:
+  agent(const std::string& args = "") {
+    std::stringstream ss("name=unknown role=unknown " + args);
+    for (std::string pair; ss >> pair;) {
+      std::string key = pair.substr(0, pair.find('='));
+      std::string value = pair.substr(pair.find('=') + 1);
+      meta[key] = {value};
+    }
+  }
+  virtual ~agent() {}
+  virtual void open_episode(const std::string& flag = "") {}
+  virtual void close_episode(const std::string& flag = "") {}
+  virtual action take_action(const board& b) { return action(); }
+  virtual bool check_for_win(const board& b) { return false; }
 
-public:
-	virtual std::string property(const std::string& key) const { return meta.at(key); }
-	virtual void notify(const std::string& msg) { meta[msg.substr(0, msg.find('='))] = { msg.substr(msg.find('=') + 1) }; }
-	virtual std::string name() const { return property("name"); }
-	virtual std::string role() const { return property("role"); }
+ public:
+  virtual std::string property(const std::string& key) const {
+    return meta.at(key);
+  }
+  virtual void notify(const std::string& msg) {
+    meta[msg.substr(0, msg.find('='))] = {msg.substr(msg.find('=') + 1)};
+  }
+  virtual std::string name() const { return property("name"); }
+  virtual std::string role() const { return property("role"); }
 
-protected:
-	typedef std::string key;
-	struct value {
-		std::string value;
-		operator std::string() const { return value; }
-		template<typename numeric, typename = typename std::enable_if<std::is_arithmetic<numeric>::value, numeric>::type>
-		operator numeric() const { return numeric(std::stod(value)); }
-	};
-	std::map<key, value> meta;
+ protected:
+  typedef std::string key;
+  struct value {
+    std::string value;
+    operator std::string() const { return value; }
+    template <typename numeric,
+              typename = typename std::enable_if<
+                  std::is_arithmetic<numeric>::value, numeric>::type>
+    operator numeric() const {
+      return numeric(std::stod(value));
+    }
+  };
+  std::map<key, value> meta;
 };
 
 /**
  * base agent for agents with randomness
  */
 class random_agent : public agent {
-public:
-	random_agent(const std::string& args = "") : agent(args) {
-		if (meta.find("seed") != meta.end())
-			engine.seed(int(meta["seed"]));
-	}
-	virtual ~random_agent() {}
+ public:
+  random_agent(const std::string& args = "") : agent(args) {
+    if (meta.find("seed") != meta.end()) engine.seed(int(meta["seed"]));
+  }
+  virtual ~random_agent() {}
 
-protected:
-	std::default_random_engine engine;
+ protected:
+  std::default_random_engine engine;
 };
 
 /**
@@ -71,30 +79,30 @@ protected:
  * put a legal piece randomly
  */
 class player : public random_agent {
-public:
-	player(const std::string& args = "") : random_agent("name=random role=unknown " + args),
-		space(board::size_x * board::size_y), who(board::empty) {
-		if (name().find_first_of("[]():; ") != std::string::npos)
-			throw std::invalid_argument("invalid name: " + name());
-		if (role() == "black") who = board::black;
-		if (role() == "white") who = board::white;
-		if (who == board::empty)
-			throw std::invalid_argument("invalid role: " + role());
-		for (size_t i = 0; i < space.size(); i++)
-			space[i] = action::place(i, who);
-	}
+ public:
+  player(const std::string& args = "")
+      : random_agent("name=random role=unknown " + args),
+        space(board::size_x * board::size_y),
+        who(board::empty) {
+    if (name().find_first_of("[]():; ") != std::string::npos)
+      throw std::invalid_argument("invalid name: " + name());
+    if (role() == "black") who = board::black;
+    if (role() == "white") who = board::white;
+    if (who == board::empty)
+      throw std::invalid_argument("invalid role: " + role());
+    for (size_t i = 0; i < space.size(); i++) space[i] = action::place(i, who);
+  }
 
-	virtual action take_action(const board& state) {
-		std::shuffle(space.begin(), space.end(), engine);
-		for (const action::place& move : space) {
-			board after = state;
-			if (move.apply(after) == board::legal)
-				return move;
-		}
-		return action();
-	}
+  virtual action take_action(const board& state) {
+    std::shuffle(space.begin(), space.end(), engine);
+    for (const action::place& move : space) {
+      board after = state;
+      if (move.apply(after) == board::legal) return move;
+    }
+    return action();
+  }
 
-private:
-	std::vector<action::place> space;
-	board::piece_type who;
+ private:
+  std::vector<action::place> space;
+  board::piece_type who;
 };
